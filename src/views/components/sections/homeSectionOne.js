73762b1293f808bd683a930a6395dd39ec754ed0
amandaflores <amandaflores.br@gmail.com>
2021-08-08 01:18:52 -0300

    updated homeSectionOne

--- src/views/components/sections/homeSectionOne.js
+++ src/views/components/sections/homeSectionOne.js
@@ -3,13 +3,11 @@ import BackgroundOne from '../../../assets/images/background/bg1.png';
 
 let HomeSectionOne = `
 <section class='section section__background--one' style="background-image: url('${BackgroundOne}')" id='section-one'>
-    <div>
-        <div class='container section__block--one'>
-            <h1>CLIQUE NO BABY YODA, ELE TE MOSTRARÁ O CAMINHO</h1>
-            <img src=${Logo} alt="Logo Star Wars" width="300px" height="auto">
-        </div>
-    </div>
+    <div class='container section__block--one'>
+        <h1>CLIQUE NO BABY YODA, ELE TE MOSTRARÁ O CAMINHO</h1>
+        <img src=${Logo} alt="Logo Star Wars">
+    </div>    
 </section>
-`
+`;
 
 export default HomeSectionOne;
